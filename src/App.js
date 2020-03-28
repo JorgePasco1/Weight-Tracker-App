9acdf134f06c356d020938852327df5c4da4b5c0
Jorge Pasco <jorgepasco@Jorges-MacBook-Pro.local>
2020-03-27 22:34:15 -0500

    implement react-firebaseUi auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from "./firebaseConfig";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+const uiConfig = {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID
+  ],
+  signInFlow: "popup"
+};
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isSignedIn: false
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user =>
+      this.setState(prevState => ({
+        ...prevState,
+        isSignedIn: !!user
+      }))
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <>
+            <div>Signed In</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+          </>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAf04_F2GESer6ydTVRZn_CCJBRFEnNxRg",
+  authDomain: "jorgepasco-weight-track-app.firebaseapp.com",
+  databaseURL: "https://jorgepasco-weight-track-app.firebaseio.com",
+  projectId: "jorgepasco-weight-track-app",
+  storageBucket: "jorgepasco-weight-track-app.appspot.com",
+  messagingSenderId: "779688991076",
+  appId: "1:779688991076:web:f8649a6753a2f2feb18271",
+  measurementId: "G-V06NKSPQJJ"
+}
+
+export default firebaseConfig;
