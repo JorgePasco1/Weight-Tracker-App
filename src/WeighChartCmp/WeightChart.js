306ed44b8be5e16472ee06adbcb491f29691351d
Jorge Pasco <jorgepasco@Jorges-MacBook-Pro.local>
2020-03-30 02:32:00 -0500

    sort data and format dates

--- src/WeighChartCmp/WeightChart.js
+++ src/WeighChartCmp/WeightChart.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Chart from "chart.js";
 import classes from "./WeightChart.module.css";
+import moment from "moment";
 
 let myLineChart;
 
@@ -26,17 +27,26 @@ export default class WeightChart extends Component {
   buildChart = () => {
     const weightChartRef = this.chartRef.current.getContext("2d");
     const { chartLabels, chartData } = this.props;
+    const chartDataMap = new Map();
+    for (let i = 0; i < chartLabels.length; i++) {
+      chartDataMap.set(chartLabels[i], chartData[i]);
+    }
+    const sortedChartDataMap = new Map([...chartDataMap.entries()].sort());
+    const labelsArray = Array.from(sortedChartDataMap.keys()).map(date => moment(date).format("MMMM Do YY"));
+    const dataArray = Array.from(sortedChartDataMap.values());
+
 
     if (typeof myLineChart !== "undefined") myLineChart.destroy();
 
     myLineChart = new Chart(weightChartRef, {
       type: "line",
       data: {
-        labels: chartLabels,
+        labels: labelsArray,
+        // labels: formattedAndSortedLabels,
         datasets: [
           {
             label: "Weight Records",
-            data: chartData,
+            data: dataArray,
             fill: false,
             borderColor: "#0F52BA"
           }
